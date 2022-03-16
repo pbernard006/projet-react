78018edb1936d7e5846ac85fcd01eb26729e6375
hugobarsacq <hugo.barsacq.4O@gmail.com>
2022-03-16 15:32:01 +0100

    Bug favorites

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,79 +1,59 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { favoritesSlice } from "../slices";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { favoritesSlice } from '../slices';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Score({ data, id }) {
-  const [favorite, setFavorite] = useState(false);
+    const [favorite, setFavorite] = useState(false);
+    const favorites = useSelector((state) => state.favorites);
 
-  const dispatch = useDispatch();
-  const addToFavorite = (event) => {
-    setFavorite(!favorite);
-    event.preventDefault();
-    dispatch(favoritesSlice.actions.toggle({ id }));
-  };
+    function getFavorite() {
+        if (favorites.includes(id)) {
+            setFavorite(true);
+        }
+    }
 
-  console.log({ data });
-  return (
-    <div className="container-fluid bg-match pt-5rem">
-      <div className="row">
-        <div className="col-12">
-          <div className="text-end">
-            <i
-              className={`${favorite ? "fas" : "far"} fa-heart fs-2 mt-2 red`}
-              onClick={addToFavorite}
-            ></i>
-          </div>
+    const dispatch = useDispatch();
+    const addToFavorite = (event) => {
+        setFavorite(!favorite);
+        event.preventDefault();
+        dispatch(favoritesSlice.actions.toggle({ id }));
+    };
+
+    useEffect(() => {
+        getFavorite();
+    }, []);
+    return (
+        <div className="container-fluid bg-match pt-5rem">
+            <div className="row">
+                <div className="col-12">
+                    <div className="text-end">
+                        <i className={`${favorite ? 'fas' : 'far'} fa-heart fs-2 mt-2 red`} onClick={addToFavorite}></i>
+                    </div>
+                </div>
+                <div className="col-6 mt-1 mb-4 text-center">
+                    {data?.teams.home.name === 'Paris Saint Germain' && <span className="text-light fs-2">PSG</span>}
+                    {data?.teams.home.name === 'Stade Brestois 29' && <span className="text-light fs-2">Stade Brestois</span>}
+                    {data?.teams.home.name !== 'Paris Saint Germain' && data?.teams.home.name !== 'Stade Brestois 29' && <span className="text-light fs-2">{data?.teams.home.name}</span>}
+                    <br />
+                    <div className="score">
+                        {data?.fixture.status.short === 'NS' && <span className="text-light fs-2">0</span>}
+                        {data?.fixture.status.short !== 'NS' && <span className="text-light fs-2">{data?.score.fulltime.home}</span>}
+                    </div>
+                </div>
+                <div className="col-6 mt-1 mb-4 text-center">
+                    {data?.teams.away.name === 'Paris Saint Germain' && <span className="text-light fs-2">PSG</span>}
+                    {data?.teams.away.name === 'Stade Brestois 29' && <span className="text-light fs-2">Stade Brestois</span>}
+                    {data?.teams.away.name !== 'Paris Saint Germain' && data?.teams.away.name !== 'Stade Brestois 29' && <span className="text-light fs-2">{data?.teams.away.name}</span>}
+                    <br />
+                    <div className="score">
+                        {data?.fixture.status.short === 'NS' && <span className="text-light fs-2">0</span>}
+                        {data?.fixture.status.short !== 'NS' && <span className="text-light fs-2">{data?.score.fulltime.away}</span>}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-6 mt-1 mb-4 text-center">
-          {data?.teams.home.name === "Paris Saint Germain" && (
-            <span className="text-light fs-2">PSG</span>
-          )}
-          {data?.teams.home.name === "Stade Brestois 29" && (
-            <span className="text-light fs-2">Stade Brestois</span>
-          )}
-          {data?.teams.home.name !== "Paris Saint Germain" &&
-            data?.teams.home.name !== "Stade Brestois 29" && (
-              <span className="text-light fs-2">{data?.teams.home.name}</span>
-            )}
-          <br />
-          <div className="score">
-            {data?.fixture.status.short === "NS" && (
-              <span className="text-light fs-2">0</span>
-            )}
-            {data?.fixture.status.short !== "NS" && (
-              <span className="text-light fs-2">
-                {data?.score.fulltime.home}
-              </span>
-            )}
-          </div>
-        </div>
-        <div className="col-6 mt-1 mb-4 text-center">
-          {data?.teams.away.name === "Paris Saint Germain" && (
-            <span className="text-light fs-2">PSG</span>
-          )}
-          {data?.teams.away.name === "Stade Brestois 29" && (
-            <span className="text-light fs-2">Stade Brestois</span>
-          )}
-          {data?.teams.away.name !== "Paris Saint Germain" &&
-            data?.teams.away.name !== "Stade Brestois 29" && (
-              <span className="text-light fs-2">{data?.teams.away.name}</span>
-            )}
-          <br />
-          <div className="score">
-            {data?.fixture.status.short === "NS" && (
-              <span className="text-light fs-2">0</span>
-            )}
-            {data?.fixture.status.short !== "NS" && (
-              <span className="text-light fs-2">
-                {data?.score.fulltime.away}
-              </span>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-    /* 
+        /*
         <div className="container-fluid bg-match">
             <div className="row">
                 <div className="col-6 mt-4  mb-4 text-center">
@@ -93,7 +73,7 @@ function Score({ data, id }) {
             </div>
         </div>
         */
-  );
+    );
 }
 
 export default Score;
