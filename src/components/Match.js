a567243205c38b21ead8b54b9934467bc04eecc9
hugobarsacq <hugo.barsacq.4O@gmail.com>
2022-03-16 11:27:29 +0100

    Utilisation du .env pour stocker les variables nécessaires au fonctionnement de l'api + Modification pour que les noms longs d'équipes deviennent petit + Début page de détail d'un match

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,10 +1,58 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Match() {
     const { id } = useParams();
-    console.log(id);
+    const apiURL = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?id=' + id;
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    return <div>coucou</div>;
+    const getData = async () => {
+        setLoading(true);
+        const response = await fetch(apiURL, {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-host': `${process.env.REACT_APP_API_HOST}`,
+                'x-rapidapi-key': `${process.env.REACT_APP_API_KEY}`,
+            },
+        });
+        const dt = await response.json();
+        console.log(dt);
+        setData(dt.response[0]);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div className="container px-2">
+            {loading ? (
+                <div className=" mt-5 d-flex justify-content-center ">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            ) : (
+                // Score
+                <div className="match-card">
+                    <div className="row">
+                        <div className="col-6 text-center">
+                            <span className="text-light">{data?.teams.home.name}</span>
+                            <br />
+                            <span className="text-light">{data?.goals.home}</span>
+                        </div>
+                        <div className="col-6 text-center">
+                            <span className="text-light">{data?.teams.away.name}</span>
+                            <br />
+                            <span className="text-light">{data?.goals.away}</span>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Match;
