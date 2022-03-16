4367b8b92b4bc0902dc503fb09d76a0783638ee5
hugobarsacq <hugo.barsacq.4O@gmail.com>
2022-03-16 12:07:20 +0100

    Création composant Lineup

---
+++ src/components/Lineup.js
@@ -0,0 +1,57 @@
+function Lineup() {
+    return (
+      <div className="mt-4">
+        <span className="lineup-informations">
+          Formation : 4-2-3-1 / Entraineur : H.Barsacq
+        </span>
+        <div className="lineup py-5">
+          <div className="text-center row">
+            <div className="col-4">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <div className="col-4">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <div className="col-4">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="col-6">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <div className="col-6">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <br />
+            <div className="col-12">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="col-3">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <div className="col-3">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <div className="col-3">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <div className="col-3">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+            <br />
+            <br />
+            <div className="col-12">
+              <span className="player-name px-1 py-1 fs-6">Mbappe</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Lineup;
